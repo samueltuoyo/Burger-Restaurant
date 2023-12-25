bd4ddedb7de39c3a69690460afba170bb1e6cad9
Tuoyo Samuel <150522328+tuoyosamuel@users.noreply.github.com>
2023-12-25 21:30:21 +0100

    Update index.js

--- Js/index.js
+++ Js/index.js
@@ -1,26 +1,4 @@
 
-//variables
-const header = document.getElementById('header');
-const mobileNav = document.getElementById('mobilenav');
-
-
-//scroll function
-let previousScroll = window.scrollY;
-
-window.addEventListener('scroll', () => {
-  
-const currentScroll = window.scrollY;
-
-if(currentScroll > previousScroll){
-  header.style.top = '-80px';
-  mobileNav.style.bottom = 0;
-  mainsec.style.padding = 0;
-} else if (currentScroll < previousScroll) {header.style.top = 0;
- mobileNav.style.bottom = '-80px';
-}
-
-previousScroll = currentScroll;
-})
 
 
 // Add click event listener to each button with a unique ID
